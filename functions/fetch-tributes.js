ebd16a53890c21475f32eb2b4a096674ddade77f
David Tamakloe <stdavidgh@gmail.com>
2020-06-04 19:35:30 +0000

    tweaked function

--- functions/fetch-tributes.js
+++ functions/fetch-tributes.js
@@ -20,6 +20,6 @@ exports.handler = async (event) => {
     const response = await fetch(url, opts);
     return {
         statusCode: 200,
-        body: JSON.stringify(response.data),
+        body: JSON.stringify(response),
     };
 };
